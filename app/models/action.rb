ba619f53586960a61be9d9061b6f8ba4f7257bbd
17nak1 <akhavan.nazila@gmail.com>
2025-04-12 16:06:45 -0400

    Add the Action model and controller action model saves the action data deposit in case of action type Transfer

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,4 +1,17 @@
 class Action < ApplicationRecord
   belongs_to :parent_transaction, class_name: 'Transaction', foreign_key: 'transaction_id'
   serialize :data, coder: JSON
+
+  before_save :extract_deposit
+
+  private
+
+  # Extracts the 'deposit' value from the 'data' hash if the action is a "Transfer" and
+  # save it in the 'transfer_deposit' attribute. Saving this value allows for easier and
+  # less computationally expensive access later, avoiding repeated parsing of the 'data' hash.
+  def extract_deposit
+    if action_type == "Transfer" && data.is_a?(Hash) && data["deposit"]
+      self.transfer_deposit = data["deposit"]
+    end
+  end
 end
