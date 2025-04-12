a8844df9eddb5d385d434cfc175428b451d2a80a
17nak1 <akhavan.nazila@gmail.com>
2025-04-12 14:23:23 -0400

    Add routes for transactions index and fetch

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  root 'transactions#index'
+  get '/fetch_transactions', to: 'transactions#fetch_transactions'
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
